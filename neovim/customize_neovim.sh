ee9bfe3d32796444d4c972c58433c4160bd54098
Enigma <chaosdelegation@gmail.com>
2023-07-13 16:55:24 +0800

    fix(neovim): add missing `function` keyword

--- neovim/customize_neovim.sh
+++ neovim/customize_neovim.sh
@@ -245,7 +245,7 @@ function confirm_without_default() {
 
 # Get the latest release version of the repository on GitHub.
 # Refer to: https://gist.github.com/lukechilds/a83e1d7127b78fef38c2914c4ececc3c
-get_latest_release() {
+function get_latest_release() {
   curl --silent "https://api.github.com/repos/$1/releases/latest" |  # Get latest release from GitHub api
     grep '"tag_name":' |  # Get tag line
     sed -E 's/.*"([^"]+)".*/\1/'  # Pluck JSON value
