92c56df7daf8204fe685645c0e042fadaa210919
Enigma <chaosdelegation@gmail.com>
2023-07-23 09:34:01 +0800

    style(terminal): change to double quotes

--- terminal/wezterm/wezterm.lua
+++ terminal/wezterm/wezterm.lua
@@ -12,20 +12,19 @@ launch_menu = {}
 
 if wezterm.target_triple == "x86_64-pc-windows-msvc" then
   table.insert(launch_menu, {
-    label = 'PowerShell',
-    args = { 'powershell.exe', '-NoLogo' },
+    label = "PowerShell",
+    args = { "powershell.exe", "-NoLogo" },
   })
-  -- config.default_prog = { "wsl" }
   config.wsl_domains = {
     {
       -- The name of this specific domain.  Must be unique amonst all types
       -- of domain in the configuration file.
-      name = 'WSL:Ubuntu-20.04',
+      name = "WSL:Ubuntu-20.04",
 
       -- The name of the distribution.  This identifies the WSL distribution.
       -- It must match a valid distribution from your `wsl -l -v` output in
       -- order for the domain to be useful.
-      distribution = 'Ubuntu-20.04',
+      distribution = "Ubuntu-20.04",
 
       -- The username to use when spawning commands in the distribution.
       -- If omitted, the default user for that distribution will be used.
@@ -33,11 +32,11 @@ if wezterm.target_triple == "x86_64-pc-windows-msvc" then
       -- username = "hunter",
 
       -- The current working directory to use when spawning commands, if
-      -- the SpawnCommand doesn't otherwise specify the directory.
+      -- the SpawnCommand doesn"t otherwise specify the directory.
 
       default_cwd = "~",
 
-      -- The default command to run, if the SpawnCommand doesn't otherwise
+      -- The default command to run, if the SpawnCommand doesn"t otherwise
       -- override it.  Note that you may prefer to use `chsh` to set the
       -- default shell for your user inside WSL to avoid needing to
       -- specify it here
