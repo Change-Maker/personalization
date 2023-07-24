e81620f669d618d418205404d7ab70b9bccaddf5
Enigma <chaosdelegation@gmail.com>
2023-07-24 10:44:10 +0800

    refactor(terminal): fine-tune tab bar

--- terminal/wezterm/tab_bar.lua
+++ terminal/wezterm/tab_bar.lua
@@ -158,7 +158,6 @@ function module.apply_to_config(config)
   config.tab_bar_at_bottom = true
   config.use_fancy_tab_bar = false
   config.show_new_tab_button_in_tab_bar = false
-  config.hide_tab_bar_if_only_one_tab = true
 end
 
 -- return our module table
