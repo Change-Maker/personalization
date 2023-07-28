f7470d736203872db9187e9f25f719fb59e7dc44
Enigma <chaosdelegation@gmail.com>
2023-07-28 16:32:34 +0800

    feat(terminal): change the colors of command palette

--- terminal/wezterm/colors.lua
+++ terminal/wezterm/colors.lua
@@ -3,16 +3,20 @@ local wezterm = require("wezterm")
 local M = {}
 local override = {
   ["Tokyo Night"] = {
-    selection_fg = "none",
-    split = "#7aa2f7",
+    colors = {
+      selection_fg = "none",
+      split = "#7aa2f7",
+    },
+    command_palette_bg_color = "#24283b",
+    command_palette_fg_color = "#a9b1d6",
   },
 }
 
-
 function M.apply_to_config(config)
   config.color_scheme = "Tokyo Night"
-  config.colors = override[config.color_scheme]
+  for k, v in pairs(override[config.color_scheme]) do
+    config[k] = v
+  end
 end
-
 
 return M
